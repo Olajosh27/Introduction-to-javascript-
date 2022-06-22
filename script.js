aabe494b0341ffe48131d06206da0cb9350471f6
Olaniyi Olanrewaju <96597292+Olajosh27@users.noreply.github.com>
2022-06-22 00:20:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const num1 =+prompt("Enter first number: ");
+const operator =prompt("Enter operator(+,-,*,/)");
+const num2 =+prompt("Enter second number: ");
+
+if(operator == '+')(result = num1 + num2);
+else if(operator == '-')(result = num1 - num2);
+else if(operator == '*')(result = num1 * num2);
+else if(operator == '/')(result = num1 / num2);
+document.write(num1 + operator + num2 +'=' + result)
